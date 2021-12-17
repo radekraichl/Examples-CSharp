7d6a10354e1d9f135038a1129ec7b324741a1b3f
Radek Raichl <radekraichl@gmail.com>
2021-12-17 21:22:48 +0100

    uprava CircularArrayTest

--- CircularArrayTest/Program.cs
+++ CircularArrayTest/Program.cs
@@ -6,15 +6,15 @@ namespace CircularArrayTest
     {
         static void Main()
         {
-            var myCircuralList = new CircularArray<int>(3);
-            myCircuralList[0] = 1;
-            myCircuralList[1] = 2;
-            myCircuralList[2] = 3;
+            var myCircuralArr = new CircularArray<int>(3);
+            myCircuralArr[0] = 1;
+            myCircuralArr[1] = 2;
+            myCircuralArr[2] = 3;
 
             for (int i = 0; i < 10; i++)
             {
-                Console.WriteLine(myCircuralList.Value);
-                myCircuralList.Next();
+                Console.WriteLine(myCircuralArr.Value);
+                myCircuralArr.Next();
             }
         }
     }
