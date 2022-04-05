c028292d4bca95d1fb0bd9ae8636767bb8effafa
Radek Raichl <radekraichl@gmail.com>
2022-04-05 17:35:15 +0200

    Add EventAction

---
+++ EventAction/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace EventAction
+{
+    class Program
+    {
+        private static int count = 0;
+        private static event Action EnterPressed;
+
+        static void Main()
+        {
+            ConsoleKeyInfo keyInfo;
+
+            EnterPressed += OnEnterPressed;
+            EnterPressed += () => count++;
+
+            do
+            {
+                keyInfo = Console.ReadKey();
+
+                if (keyInfo.Key == ConsoleKey.Enter)
+                {
+                    EnterPressed?.Invoke();
+                }
+            }
+            while (keyInfo.Key != ConsoleKey.Escape);
+        }
+
+        private static void OnEnterPressed()
+        {
+            Console.WriteLine($"ENTER pressed ({count})");
+        }
+    }
+}
