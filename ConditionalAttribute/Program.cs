074383811781c8546608b3bc68af1aea4facc2f0
Radek Raichl <radekraichl@gmail.com>
2022-04-09 19:45:24 +0200

    Add project ConditionalAttribute

---
+++ ConditionalAttribute/Program.cs
@@ -0,0 +1,29 @@
+﻿#define Debug
+
+using System.Diagnostics;
+
+namespace ConditionalAttributeTest;
+
+internal class Program
+{
+    static void Main()
+    {
+
+#if Debug
+        Console.WriteLine("Debug mode is active");
+#endif
+
+        int number = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            number += 2;
+            DebugNumber(number);
+        }
+
+        [Conditional("Debug")]
+        static void DebugNumber(int num)
+        {
+            Console.WriteLine($"number = {num}");
+        }
+    }
+}
