0c191a36f8978034cea09f8e989f7e54144061cb
Radek Raichl <radekraichl@gmail.com>
2022-04-09 19:59:26 +0200

    Add OverflowVariable project

---
+++ OverflowVariable/Program.cs
@@ -0,0 +1,23 @@
+﻿byte variable = default;
+
+for (int i = 0; i < byte.MaxValue + 1; i++)
+{
+    checked
+    {
+        try
+        {
+            variable++;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+
+    if (variable == 0)
+    {
+        Console.WriteLine("Overflow !");
+    }
+}
+
+Console.WriteLine(variable);
