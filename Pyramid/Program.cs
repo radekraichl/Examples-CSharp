95591b90f9e1169b28a40b12f8ad81a22d5fe3e3
Radek Raichl <radekraichl@gmail.com>
2022-05-18 21:02:01 +0200

    Add Pyramid project

---
+++ Pyramid/Program.cs
@@ -0,0 +1,21 @@
+﻿int peak = 8;
+
+for (int i = 1; i <= peak; i++)
+{
+    PrintLine(i);
+}
+
+for (int i = peak - 1; i >= 1; i--)
+{
+    PrintLine(i);
+}
+
+static void PrintLine(int number)
+{
+    for (int j = 1; j <= number; j++)
+    {
+        Console.Write($"{j} ");
+    }
+
+    Console.WriteLine();
+}
